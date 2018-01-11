c3312f009223ef8b7a399857caac748fcdcc53c1
Alyssa Clair <clairaly13@gmail.com>
2018-01-11 21:01:43 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,13 +15,15 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT name, age, SUM(pledges.amount) AS amount FROM users
-  JOIN pledges
-  ON users.id = pledges.user_id
-  GROUP BY users.name;"
+    JOIN pledges
+    ON users.id = pledges.user_id
+    GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT title, funding_goal, SUM(pledges.amount) FROM projects
+  JOIN pledges
+  ON projects.id = pledges.amount"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
